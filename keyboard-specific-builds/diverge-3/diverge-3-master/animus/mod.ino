80970d8bbfa4f12e346e3ad7ea17561afe169b60
sunjayc <sunjayc@Suyodhana>
2017-12-13 01:08:49 -0800

    hook up mod_shifted

--- keyboard-specific-builds/diverge-3/diverge-3-master/animus/mod.ino
+++ keyboard-specific-builds/diverge-3/diverge-3-master/animus/mod.ino
@@ -1,8 +1,8 @@
-#define builder_mstartup BledPWMStartup(); I2CStartup(); mediaStartup(); macroStartup(); dualRolesStartup(); tapDanceStartup(); mouseKeysStartup(); stickyKeysStartup();
-#define builder_mloop BledPWMLoop(); I2CLoop(); mediaLoop(); macroLoop(); dualRolesLoop(); tapDanceLoop(); mouseKeysLoop(); stickyKeysLoop();
-#define builder_mkeydown BledPWMKeyDown(val, type); I2CKeyDown(val, type); mediaKeyDown(val, type); macroKeyDown(val, type); dualRolesKeyDown(val, type); tapDanceKeyDown(val, type); mouseKeysKeyDown(val, type); stickyKeysKeyDown(val, type);
-#define builder_mkeyup BledPWMKeyUp(val, type); I2CKeyUp(val, type); mediaKeyUp(val, type); macroKeyUp(val, type); dualRolesKeyUp(val, type); tapDanceKeyUp(val, type); mouseKeysKeyUp(val, type); stickyKeysKeyUp(val, type);
-#define builder_mserial BledPWMSerial(input); I2CSerial(input); mediaSerial(input); macroSerial(input); dualRolesSerial(input); tapDanceSerial(input); mouseKeysSerial(input); stickyKeysSerial(input);
+#define builder_mstartup BledPWMStartup(); I2CStartup(); mediaStartup(); shiftedStartup(); macroStartup(); dualRolesStartup(); tapDanceStartup(); mouseKeysStartup(); stickyKeysStartup();
+#define builder_mloop BledPWMLoop(); I2CLoop(); mediaLoop(); shiftedLoop(); macroLoop(); dualRolesLoop(); tapDanceLoop(); mouseKeysLoop(); stickyKeysLoop();
+#define builder_mkeydown BledPWMKeyDown(val, type); I2CKeyDown(val, type); mediaKeyDown(val, type); shiftedKeyDown(val, type); macroKeyDown(val, type); dualRolesKeyDown(val, type); tapDanceKeyDown(val, type); mouseKeysKeyDown(val, type); stickyKeysKeyDown(val, type);
+#define builder_mkeyup BledPWMKeyUp(val, type); I2CKeyUp(val, type); mediaKeyUp(val, type); shiftedKeyUp(val, type); macroKeyUp(val, type); dualRolesKeyUp(val, type); tapDanceKeyUp(val, type); mouseKeysKeyUp(val, type); stickyKeysKeyUp(val, type);
+#define builder_mserial BledPWMSerial(input); I2CSerial(input); mediaSerial(input); shiftedSerial(input); macroSerial(input); dualRolesSerial(input); tapDanceSerial(input); mouseKeysSerial(input); stickyKeysSerial(input);
 
 void ModStartup()
 {
@@ -21,6 +21,11 @@ void ModLoop()
 void ModKeyDown(char val, byte type)
 {
   builder_mkeydown
+}
+
+void ModIntercedePress(char val, byte type)
+{
+  shiftedIntercedePress(val, type);
 }
 
 void ModKeyUp(char val, byte type)
